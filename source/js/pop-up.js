8262ec51c0204e01d9442c1c0c79c92dbd5953dc
Lena Vladi <lenavladi.web@ya.ru>
2018-05-14 01:18:04 +0300

    Б28, Б29, Д7, Д11 - in progress Б8, Б11

--- source/js/pop-up.js
+++ source/js/pop-up.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const failurePopUp = document.querySelector('.failure-pop-up'),
-      succesPopUp = document.querySelector('.succes-pop-up');
+const failurePopUp = document.querySelector(".failure-pop-up"),
+      successPopUp = document.querySelector(".success-pop-up");
 
-const formFeedback = document.querySelector('.get-feedback'),
-      btn = document.querySelector('.get-feedback__request'),
-      inputs = formFeedback.querySelectorAll('input[required]');
+const formFeedback = document.querySelector(".get-feedback"),
+      btn = document.querySelector(".get-feedback__request"),
+      inputs = formFeedback.querySelectorAll("input[required]");
 
-failurePopUp.classList.remove('failure-pop-up--visible');
-succesPopUp.classList.remove('succes-pop-up--visible');
+failurePopUp.classList.remove("failure-pop-up--visible");
+successPopUp.classList.remove("success-pop-up--visible");
 
 console.log(inputs);
 
@@ -17,9 +17,9 @@ let ok = [];
 
 //подписка за событие о заполнении инпутов
 Array.from(inputs).forEach(el => {
-  el.addEventListener('change', () => {
+  el.addEventListener("change", () => {
     if (ok.length == 3) {
-      btn.classList.remove('button-disable');
+      btn.classList.remove("button-disable");
     } else {
       ok.push(el);
     }
@@ -27,24 +27,24 @@ Array.from(inputs).forEach(el => {
 })
 
 //обработка клика отправки формы -> показ модальных окон
-btn.addEventListener('click', (event) => {
+btn.addEventListener("click", (event) => {
   //event.preventDefault();
-  if (btn.classList.contains('button-disable')) {
+  if (btn.classList.contains("button-disable")) {
     return;
   } else {
-    const err = Array.from(inputs).find(el => el.value == '');
+    const err = Array.from(inputs).find(el => el.value == "");
 
     if (err) {
-      failurePopUp.classList.add('failure-pop-up--visible');
+      failurePopUp.classList.add("failure-pop-up--visible");
     } else {
-      succesPopUp.classList.add('succes-pop-up--visible');
+      successPopUp.classList.add("success-pop-up--visible");
       //функция отправки формы
     }
   }
 })
 
 //закрывает модальное окно
-failurePopUp.querySelector('.failure-pop-up__button').addEventListener('click', () => failurePopUp.classList.remove('failure-pop-up--visible'));
+failurePopUp.querySelector(".failure-pop-up__button").addEventListener("click", () => failurePopUp.classList.remove("failure-pop-up--visible"));
 
 //закрывает модальное окно
-succesPopUp.querySelector('.succes-pop-up__button').addEventListener('click', () => succesPopUp.classList.remove('succes-pop-up--visible'));
+successPopUp.querySelector(".success-pop-up__button").addEventListener("click", () => successPopUp.classList.remove("success-pop-up--visible"));
--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 //открытие/закрытие меню
 
-const nav = document.querySelector('.nav'),
-      navToggle = document.querySelector('.nav__toggle-button');
+const nav = document.querySelector(".nav"),
+      navToggle = document.querySelector(".nav__toggle-button");
 
-nav.classList.remove('nav--nojs');
-nav.classList.add('nav--closed');
+nav.classList.remove("nav--nojs");
+nav.classList.add("nav--closed");
 
-navToggle.addEventListener('click', function() {
-  if (nav.classList.contains('nav--closed')) {
-    nav.classList.remove('nav--closed');
+navToggle.addEventListener("click", function() {
+  if (nav.classList.contains("nav--closed")) {
+    nav.classList.remove("nav--closed");
   } else {
-    nav.classList.add('nav--closed');
+    nav.classList.add("nav--closed");
   }
 });
